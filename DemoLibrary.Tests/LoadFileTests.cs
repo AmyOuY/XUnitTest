ed874390913c880494297e75ddab250aa9b8c661
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2020-06-04 18:26:25 -0400

    Finish tests for LoadFile class

---
+++ DemoLibrary.Tests/LoadFileTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class LoadFileTests
+    {
+        [Fact]
+        public void Load_ValidNameShouldWork()
+        {
+            string actual = LoadFile.Load("It is a valid file name");
+
+            Assert.True(actual.Length > 0);
+        }
+
+
+        //[Fact]
+        //public void Load_InvalidNameShouldFail()
+        //{
+        //    Assert.Throws<FileNotFoundException>(() => LoadFile.Load(" "));
+        //}
+
+
+        [Fact]
+        public void Load_InvalidNameShouldFail()
+        {
+            Assert.Throws<ArgumentException>("file", () => LoadFile.Load(" "));
+        }
+
+    }
+}
---
+++ DemoLibrary/LoadFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class LoadFile
+    {
+        public static string Load(string file)
+        {
+            if (file.Length < 10)
+            {
+                //throw new System.IO.FileNotFoundException();
+                throw new ArgumentException("The file name is too short", "file");
+            }
+
+            return "File was successfully loaded";
+        }
+    }
+}
