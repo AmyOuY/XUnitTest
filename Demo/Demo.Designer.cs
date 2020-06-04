ec3c9f80e7f1226ac63ddf293aeb40158cecdc81
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2020-06-04 16:21:46 -0400

    Finish building the Calculator UI

---
+++ Demo/Demo.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Demo
+{
+    partial class Demo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculatorLabel = new System.Windows.Forms.Label();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.xValue = new System.Windows.Forms.TextBox();
+            this.yValue = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.divideButtton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultValue = new System.Windows.Forms.TextBox();
+            this.employeeDataLabel = new System.Windows.Forms.Label();
+            this.usersLabel = new System.Windows.Forms.Label();
+            this.usersDropdown = new System.Windows.Forms.ComboBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameValue = new System.Windows.Forms.TextBox();
+            this.lastNameValue = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calculatorLabel
+            // 
+            this.calculatorLabel.AutoSize = true;
+            this.calculatorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculatorLabel.Location = new System.Drawing.Point(170, 19);
+            this.calculatorLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.calculatorLabel.Name = "calculatorLabel";
+            this.calculatorLabel.Size = new System.Drawing.Size(144, 32);
+            this.calculatorLabel.TabIndex = 0;
+            this.calculatorLabel.Text = "Calculator";
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xLabel.Location = new System.Drawing.Point(126, 107);
+            this.xLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(20, 20);
+            this.xLabel.TabIndex = 1;
+            this.xLabel.Text = "X";
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yLabel.Location = new System.Drawing.Point(126, 165);
+            this.yLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(19, 20);
+            this.yLabel.TabIndex = 2;
+            this.yLabel.Text = "Y";
+            // 
+            // xValue
+            // 
+            this.xValue.Location = new System.Drawing.Point(176, 100);
+            this.xValue.Name = "xValue";
+            this.xValue.Size = new System.Drawing.Size(135, 30);
+            this.xValue.TabIndex = 3;
+            // 
+            // yValue
+            // 
+            this.yValue.Location = new System.Drawing.Point(176, 158);
+            this.yValue.Name = "yValue";
+            this.yValue.Size = new System.Drawing.Size(135, 30);
+            this.yValue.TabIndex = 4;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(54, 251);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(108, 43);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // subtractButton
+            // 
+            this.subtractButton.Location = new System.Drawing.Point(54, 300);
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.Size = new System.Drawing.Size(108, 43);
+            this.subtractButton.TabIndex = 6;
+            this.subtractButton.Text = "Subtract";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            this.subtractButton.Click += new System.EventHandler(this.subtractButton_Click);
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(54, 349);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(108, 43);
+            this.multiplyButton.TabIndex = 7;
+            this.multiplyButton.Text = "Multiply";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyButton_Click);
+            // 
+            // divideButtton
+            // 
+            this.divideButtton.Location = new System.Drawing.Point(54, 398);
+            this.divideButtton.Name = "divideButtton";
+            this.divideButtton.Size = new System.Drawing.Size(108, 43);
+            this.divideButtton.TabIndex = 8;
+            this.divideButtton.Text = "Divide";
+            this.divideButtton.UseVisualStyleBackColor = true;
+            this.divideButtton.Click += new System.EventHandler(this.divideButtton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(277, 251);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 9;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(216, 300);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(81, 29);
+            this.resultLabel.TabIndex = 10;
+            this.resultLabel.Text = "Result";
+            // 
+            // resultValue
+            // 
+            this.resultValue.Location = new System.Drawing.Point(178, 337);
+            this.resultValue.Name = "resultValue";
+            this.resultValue.Size = new System.Drawing.Size(149, 30);
+            this.resultValue.TabIndex = 11;
+            // 
+            // employeeDataLabel
+            // 
+            this.employeeDataLabel.AutoSize = true;
+            this.employeeDataLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeDataLabel.Location = new System.Drawing.Point(620, 19);
+            this.employeeDataLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.employeeDataLabel.Name = "employeeDataLabel";
+            this.employeeDataLabel.Size = new System.Drawing.Size(209, 32);
+            this.employeeDataLabel.TabIndex = 12;
+            this.employeeDataLabel.Text = "Employee Data";
+            // 
+            // usersLabel
+            // 
+            this.usersLabel.AutoSize = true;
+            this.usersLabel.Location = new System.Drawing.Point(598, 116);
+            this.usersLabel.Name = "usersLabel";
+            this.usersLabel.Size = new System.Drawing.Size(63, 25);
+            this.usersLabel.TabIndex = 13;
+            this.usersLabel.Text = "Users";
+            // 
+            // usersDropdown
+            // 
+            this.usersDropdown.FormattingEnabled = true;
+            this.usersDropdown.Location = new System.Drawing.Point(679, 108);
+            this.usersDropdown.Name = "usersDropdown";
+            this.usersDropdown.Size = new System.Drawing.Size(235, 33);
+            this.usersDropdown.TabIndex = 14;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(598, 246);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(101, 25);
+            this.firstNameLabel.TabIndex = 15;
+            this.firstNameLabel.Text = "FirstName";
+            // 
+            // firstNameValue
+            // 
+            this.firstNameValue.Location = new System.Drawing.Point(705, 241);
+            this.firstNameValue.Name = "firstNameValue";
+            this.firstNameValue.Size = new System.Drawing.Size(209, 30);
+            this.firstNameValue.TabIndex = 16;
+            // 
+            // lastNameValue
+            // 
+            this.lastNameValue.Location = new System.Drawing.Point(705, 306);
+            this.lastNameValue.Name = "lastNameValue";
+            this.lastNameValue.Size = new System.Drawing.Size(209, 30);
+            this.lastNameValue.TabIndex = 18;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(598, 311);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(101, 25);
+            this.lastNameLabel.TabIndex = 17;
+            this.lastNameLabel.Text = "LastName";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(189, 398);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(108, 43);
+            this.clearButton.TabIndex = 19;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.lastNameValue);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameValue);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.usersDropdown);
+            this.Controls.Add(this.usersLabel);
+            this.Controls.Add(this.employeeDataLabel);
+            this.Controls.Add(this.resultValue);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.divideButtton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.subtractButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.yValue);
+            this.Controls.Add(this.xValue);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.calculatorLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Demo";
+            this.Text = "Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label calculatorLabel;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.TextBox xValue;
+        private System.Windows.Forms.TextBox yValue;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subtractButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button divideButtton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox resultValue;
+        private System.Windows.Forms.Label employeeDataLabel;
+        private System.Windows.Forms.Label usersLabel;
+        private System.Windows.Forms.ComboBox usersDropdown;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox firstNameValue;
+        private System.Windows.Forms.TextBox lastNameValue;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Demo/Demo.cs
@@ -0,0 +1,102 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Demo : Form
+    {
+        public Demo()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsValidXY()
+        {
+            if (xValue.Text.Length > 0 && yValue.Text.Length > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (IsValidXY())
+            {
+                double x = Double.Parse(xValue.Text);
+                double y = Double.Parse(yValue.Text);
+
+                double output = Calculator.Add(x, y);
+                resultValue.Text = output.ToString();
+            }
+            else
+            {
+                MessageBox.Show("You need to fill in valid X and Y value");
+            }
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            if (IsValidXY())
+            {
+                double x = Double.Parse(xValue.Text);
+                double y = Double.Parse(yValue.Text);
+
+                double output = Calculator.Subtract(x, y);
+                resultValue.Text = output.ToString();
+            }
+            else
+            {
+                MessageBox.Show("You need to fill in valid X and Y value");
+            }
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            if (IsValidXY())
+            {
+                double x = Double.Parse(xValue.Text);
+                double y = Double.Parse(yValue.Text);
+
+                double output = Calculator.Multiply(x, y);
+                resultValue.Text = output.ToString();
+            }
+            else
+            {
+                MessageBox.Show("You need to fill in valid X and Y value");
+            }
+        }
+
+        private void divideButtton_Click(object sender, EventArgs e)
+        {
+            if (IsValidXY())
+            {
+                double x = Double.Parse(xValue.Text);
+                double y = Double.Parse(yValue.Text);
+
+                double output = Calculator.Divide(x, y);
+                resultValue.Text = output.ToString();
+            }
+            else
+            {
+                MessageBox.Show("You need to fill in valid X and Y value");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            xValue.Text = "";
+            yValue.Text = "";
+            resultValue.Text = "";
+        }
+    }
+}
