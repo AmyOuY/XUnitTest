d319d84626a44178bff3585a0a12c2dc823ca4e8
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2021-09-10 16:46:26 -0400

    Add Anagram class and test cases

---
+++ DemoLibrary.Tests/AnagramTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class AnagramTest
+    {
+        
+        [Theory]
+        [InlineData("a@\uD83DB", "aB", "string 1")]
+        [InlineData("ab", "a@\uD83DB", "string 2")]
+        [InlineData("a\ud83e\udd70B", "aB", "string 1")]
+        [InlineData("ab", "a\ud83e\udd70B", "string 2")]
+        public void IsAnagram_InvalidStringShouldFail(string s1, string s2, string param)
+        {
+            Assert.Throws<ArgumentException>(param, () => Anagram.IsAnagram(s1, s2));
+        }
+
+
+        [Theory]
+        [InlineData("anagram", "gamanra")]
+        [InlineData("AnAGram", "gramana")]
+        [InlineData("anagram", "naagram")]
+        [InlineData("anagram", "anargam")]
+        [InlineData("anagram", "anagrma")]
+        [InlineData("an", "na")]
+        [InlineData("New York Times", "monkeys write")]
+        [InlineData("New, York Times", "monkeys write")]
+        [InlineData("New, York, Times", "monkeys write")]
+        [InlineData("New York Times", "monkeys, write")]
+        [InlineData("New York Times", "monkeys, write,")]
+        public void IsAnagram_ShouldWork(string s1, string s2)
+        {
+            Assert.True(Anagram.IsAnagram(s1, s2));
+        }
+
+
+        [Theory]
+        [InlineData("anagram", "cdefkx")]
+        [InlineData("anagram", "just")]
+        [InlineData("anagram", "anagram")]
+        [InlineData("anagram", "aNagram")]
+        [InlineData("aNaGram", "anagram")]
+        [InlineData("anagram", "nnagram")]
+        [InlineData("anagram", "angrram")]
+        [InlineData("anagram", "anagrmm")]
+        [InlineData("a", "a")]        
+        public void IsAnagram_ShouldFail(string s1, string s2)
+        {
+            Assert.False(Anagram.IsAnagram(s1, s2));
+        }
+    }
+}
---
+++ DemoLibrary/Anagram.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class Anagram
+    {
+        public static bool IsAnagram(string s1, string s2)
+        {
+            string s1Lower = s1.ToLower();
+            string s2Lower = s2.ToLower();
+
+            if (s1Lower.Equals(s2Lower)) return false;
+
+            int[] count1 = new int[256];
+            int[] count2 = new int[256];
+
+            foreach (char c in s1Lower)
+            {
+                if (c > 255)
+                {
+                    throw new ArgumentException("string contains non-ASCII codes", "string 1");
+                }
+
+                if (c != ' ' && c != ',')
+                {
+                    count1[c]++;
+                }  
+            }
+
+            foreach (char c in s2Lower)
+            {
+                if (c > 255)
+                {
+                    throw new ArgumentException("string contains non-ASCII codes", "string 2");
+                }
+
+                if (c != ' ' && c != ',')
+                {
+                    count2[c]++;
+                }
+                
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (count1[i] != count2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
